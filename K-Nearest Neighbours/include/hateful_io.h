bc5194bd090a6196ddce79f10a73b8ae4346e928
MahdiHatesC <mahdiracimmorsli@gmail.com>
2026-04-29 15:01:53 -0400

    Ajout d'informations utiles à la collecte de données

--- K-Nearest Neighbours/include/hateful_io.h
+++ K-Nearest Neighbours/include/hateful_io.h
@@ -3,4 +3,13 @@
 
 void lire_fichier(char* nom_fichier);
 
+enum Genre {MALE, FEMALE};
+
+struct donnee {
+    enum Genre genre;
+    int taille;
+    int poids;
+    int imc;
+} typedef t_donnee;
+
 #endif
